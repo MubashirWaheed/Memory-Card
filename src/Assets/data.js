33867ffd4ff889b66552da0e44e2e8b6e632b669
MubashirWaheed <mubashir3080@gmail.com>
2021-11-14 01:51:12 +0500

    images on click working

---
+++ src/Assets/data.js
@@ -0,0 +1,62 @@
+export const data = [
+    {
+        id: 0,
+        url: "arquious.jpg",
+        clicked: false
+    },
+    {
+        id: 1,
+        url: "bayleef.jpg",
+        clicked: false
+    },
+    {
+        id: 2,
+        url: "dedenne.jpg",
+        clicked: false
+    },
+    {
+        id: 3,
+        url: "deoxys.jpg",
+        clicked: false
+    },
+    {
+        id: 4,
+        url: "hawlucha.jpg",
+        clicked: false
+    },
+    {
+        id: 5,
+        url: "linoone.jpg",
+        clicked: false
+    },
+    {
+        id: 6,
+        url: "meganium.jpg",
+        clicked: false
+    },
+    {
+        id: 7,
+        url: "pumpkaboo.jpg",
+        clicked: false
+    },
+    {
+        id: 8,
+        url: "riolu.jpg",
+        clicked: false
+    },
+    {
+        id: 9,
+        url: "sitio.jpg",
+        clicked: false
+    },
+    {
+        id: 10,
+        url: "vulpix.jpg",
+        clicked: false
+    },
+    {
+        id: 11,
+        url: "wooper.jpg",
+        clicked: false
+    }
+]
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
+import cardscss from "../styles/Cards.module.css"
+import { data } from "../Assets/data";
 
+// Randomization of array elelmets 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 function Cards(){
+    const [imageState, setimagesState] = useState(data);
+
+    const photoClicked = ()=>{
+        let newDataSet = []
+        newDataSet = [...shuffleArray(data)]
+        setimagesState(newDataSet);
+    }
     return(
-        <div>Card</div>
+        <div className={cardscss.cards}>
+            {data.map((item)=>
+                <div className={cardscss.card} key={item.id} >
+                    <img src={require(`../images/${item.url}`).default} alt="pokemon" onClick={photoClicked} />
+                    <h4>{item.url}</h4>
+                </div>
+                )
+            }
+        </div>
     )
 }
 
